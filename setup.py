0c7f4c470ce1a864c413f0e19c5873544a182e44
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-15 13:15:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import *
 
+LONG_DESC = """
+This is pil utilities
+"""
+
+setup(name='pil_utils',
+	  version='0.0.1',
+	  description='Pil utilities',
+	  long_description=LONG_DESC,
+	  author='Sang Ki Kwon',
+	  url='https://github.com/automatethem/pil_utils',
+	  install_requires=[],
+	  author_email='automatethem@gmail.com',
+	  license='MIT',
+	  packages=find_packages(),
+	  zip_safe=False)

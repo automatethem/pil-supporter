6ec9f5df972fc43a324fad69ebf2af8bb4af0b37
automatethem <108699325+automatethem@users.noreply.github.com>
2023-02-03 12:49:41 +0900

    Update draw.py

--- pil_helper/utils/draw.py
+++ pil_helper/utils/draw.py
@@ -1,74 +1,75 @@
 from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+#matplotlib 패키지 한글 깨짐 처리 시작
 import matplotlib.pyplot as plt
 import platform
+if platform.system() == 'Darwin': #맥
+        plt.rc('font', family='AppleGothic') 
+elif platform.system() == 'Windows': #윈도우
+        plt.rc('font', family='Malgun Gothic') 
+elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+        #!wget "https://www.wfonts.com/download/data/2016/06/13/malgun-gothic/malgun.ttf"
+        #!mv malgun.ttf /usr/share/fonts/truetype/
+        #import matplotlib.font_manager as fm 
+        #fm._rebuild() 
+        plt.rc('font', family='Malgun Gothic') 
+plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+#matplotlib 패키지 한글 깨짐 처리 끝
 
 def pil_draw_rect(image, point1, point2):
-    draw = ImageDraw.Draw(image)
-    draw.rectangle((point1, point2), outline=(0, 0, 255), width=3)
-
-    return image
+    plt.figure(figsize=(16, 10))
+    #fig, ax = plt.subplots(nrows=1, ncols=1)
+    fig = plt.gcf()
+    ax = plt.gca()
+    plt.imshow(image목
+    plt.title('제목')
+    ax.add_patch(
+        patches.Rectangle(
+            (0, 0), 200, 100,
+            edgecolor='red',
+            facecolor='red',
+            linewidth=2,
+            fill=False
+        )
+    )
 
 def pil_draw_label(image, text, point, font_color=(255, 255, 255), font_size=28):
-    x, y = point
-    draw = ImageDraw.Draw(image)
-    if platform.system() == 'Darwin': #맥
-        font = 'AppleGothic.ttf'
-    elif platform.system() == 'Windows': #윈도우
-        font = 'malgun.ttf'
-    elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-        '''
-        !wget "https://www.wfonts.com/download/data/2016/06/13/malgun-gothic/malgun.ttf"
-        !mv malgun.ttf /usr/share/fonts/truetype/
-        import matplotlib.font_manager as fm 
-        fm._rebuild() 
-        '''
-        font = 'malgun.ttf'
-    try:
-        imageFont = ImageFont.truetype(font, font_size)
-    except:
-        imageFont = ImageFont.load_default()
-    text_width, text_height = imageFont.getsize(text)
-    draw.text((x, y - text_height), text, font=imageFont, fill=font_color)
-
-    return image
+    plt.figure(figsize=(16, 10))
+    #fig, ax = plt.subplots(nrows=1, ncols=1)
+    fig = plt.gcf()
+    ax = plt.gca()
+    plt.imshow(image목
+    plt.title('제목')
+    ax.text(200, 200, '텍스트1')
 
 def pil_draw_rect_with_label(image, point1, point2, text, font_color=(255, 255, 255), font_size=28):
-    draw = ImageDraw.Draw(image)
-    draw.rectangle((point1, point2), outline=(0, 0, 255), width=3)
-
-    x1, y1 = point1
-    if platform.system() == 'Darwin': #맥
-        font = 'AppleGothic.ttf'
-    elif platform.system() == 'Windows': #윈도우
-        font = 'malgun.ttf'
-    elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-        '''
-        !wget "https://www.wfonts.com/download/data/2016/06/13/malgun-gothic/malgun.ttf"
-        !mv malgun.ttf /usr/share/fonts/truetype/
-        import matplotlib.font_manager as fm 
-        fm._rebuild() 
-        '''
-        font = 'malgun.ttf'
-    try:
-        imageFont = ImageFont.truetype(font, font_size)
-    except:
-        imageFont = ImageFont.load_default()
-    text_width, text_height = imageFont.getsize(text)
-    draw.rectangle(((x1, y1 - text_height), (x1 + text_width, y1)), fill=(0, 0, 255)) #채워진 사각형
-    draw.text((x1, y1 - text_height), text, font=imageFont, fill=font_color)
-
-    return image
+    plt.figure(figsize=(16, 10))
+    #fig, ax = plt.subplots(nrows=1, ncols=1)
+    fig = plt.gcf()
+    ax = plt.gca()
+    plt.imshow(image)
+    x, y, width, height = bbox
+    ax.add_patch(
+        patches.Rectangle(
+            (x, y), width, height,
+            edgecolor='red',
+            facecolor='red',
+            linewidth=2,
+            fill=False
+        )
+    )
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
 
 def pil_draw_point(image, point):
-    x, y = point
-    draw = ImageDraw.Draw(image)
-    radius = 2
-    draw.ellipse((x - radius, y - radius, x + radius, y + radius), fill=(0, 0, 255))
-
-    return image
+    plt.figure(figsize=(16, 10))
+    #fig, ax = plt.subplots(nrows=1, ncols=1)
+    fig = plt.gcf()
+    ax = plt.gca()
+    plt.imshow(image목
+    plt.title('제목')
 
 def pil_draw_image(image, image_to_draw, point):
     alpha_range = image_to_draw.getextrema()[-1]

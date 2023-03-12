c8f03078eb59ce275b0abb7402ce771cf26c34c5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-12 13:16:40 +0900

    Update __init__.py

--- pil_helper/utils/__init__.py
+++ pil_helper/utils/__init__.py
@@ -1,17 +1,5 @@
 from . import draw
-
-'''
-import pil_helper
-
-pil_helper.utils.draw_rect
-pil_helper.utils.draw_label
-pil_helper.utils.draw_rect_with_label
-pil_helper.utils.draw_point
-pil_helper.utils.draw_image
-pil_helper.utils.crop_center
-pil_helper.utils.create_dummy_image
-pil_helper.utils.read_from_image_file
-'''
+from . import etc
 
 from .draw import draw_rect
 from .draw import draw_label

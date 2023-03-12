af8d49b505863d76ae5eb1fb00b446dc68e1e243
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-12 13:16:07 +0900

    Update __init__.py

--- pil_helper/utils/__init__.py
+++ pil_helper/utils/__init__.py
@@ -8,6 +8,9 @@ pil_helper.utils.draw_label
 pil_helper.utils.draw_rect_with_label
 pil_helper.utils.draw_point
 pil_helper.utils.draw_image
+pil_helper.utils.crop_center
+pil_helper.utils.create_dummy_image
+pil_helper.utils.read_from_image_file
 '''
 
 from .draw import draw_rect
@@ -15,3 +18,6 @@ from .draw import draw_label
 from .draw import draw_rect_with_label
 from .draw import draw_point
 from .draw import draw_image
+from .etc import crop_center
+from .etc import create_dummy_image
+from .etc import read_from_image_file
